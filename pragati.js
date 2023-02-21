b558f624a0ff326bdbe52fe461e8a6663e9b9386
Pandav-College <radhikataipandavcollegeofengg@gmail.com>
2023-02-21 20:32:33 +0530

    Object added

--- pragati.js
+++ pragati.js
@@ -1,1 +1,19 @@
 console.log(`hello from pragati`);
+
+let obj = {
+    id: 123,
+    title: "Pragati Says",
+    category: 34,
+    date: "2023-02-20",
+    image: "http://www.destinyhub.info/app_dashboard/uploads/62413.jfif",
+    description: "",
+    category_name: "Pragati test",
+    isProfessor : true,
+    family : {
+        fatherName : `Someshwar`,
+        age : 70,
+        motherName : `Lalita`,
+        isTeacher : false
+      }
+  }
+  console.log(obj.family[`motherName`]);
